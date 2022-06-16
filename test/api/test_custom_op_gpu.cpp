788954b0721ea9670f15c1cf9ab9988c1f597f1d
umangyadav <umayadav@amd.com>
2022-06-16 14:33:30 -0500

    formatting

--- test/api/test_custom_op_gpu.cpp
+++ test/api/test_custom_op_gpu.cpp
@@ -84,7 +84,7 @@ TEST_CASE(run_simple_custom_op)
     std::vector<float> x_data(12, 1);
     std::vector<float> ret_data(12, -1);
     pp.add("x", migraphx::argument(s, x_data.data()));
-    auto result                       = p.eval(pp)[0];
+    auto result                        = p.eval(pp)[0];
     std::vector<float> expected_result = {0, 0, -1, -1, 0, 0, -1, -1, 0, 0, -1, -1};
     auto result_vec                    = result.as_vector<float>();
     EXPECT(migraphx::verify_range(result_vec, expected_result));
